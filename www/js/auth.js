cb62fa646f4272aafa11efcfc707c491a228cdb6
daydreamheart <m4gy4r.m3l1nd4@gmail.com>
2021-04-22 15:30:26 +0900

    Implemented log in (with validation & error handling) + log out functionalities.

---
+++ www/js/auth.js
@@ -0,0 +1,33 @@
+// Listen for Auth status changes
+auth.onAuthStateChanged(user => {
+  console.log(user);
+});
+
+// Logout
+const logoutBtn = document.querySelector('#logout');
+logoutBtn.addEventListener('click', () => {
+  auth.signOut();
+});
+
+// Login
+const signInForm = document.querySelector('#signin-form');
+const signInBtn = document.querySelector('#sign-in');
+
+signInBtn.addEventListener('click', () => {
+  if (app.input.validate('#signin-email') && app.input.validate('#signin-password')) {
+    // Get user info
+    const email = signInForm['signin-email'].value;
+    const password = signInForm['signin-password'].value;
+
+    // Sign in the user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+      // Close the sign in popup and reset the form
+      app.popup.close('.login-popup');
+      signInForm.reset();
+      signInForm.querySelector('.error').innerHTML = '';
+    }).catch(error => {
+      signInForm.querySelector('.error').innerHTML = error.message;
+    });;
+
+  }
+});

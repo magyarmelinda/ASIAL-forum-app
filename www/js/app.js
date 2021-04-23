05ccaa626163121421e29767ed3e92ae8bb1d808
daydreamheart <m4gy4r.m3l1nd4@gmail.com>
2021-04-23 15:16:50 +0900

    Set up Thread Details function.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,9 @@ var app = new Framework7({
   name: 'framework7-core-tab-view', // App name
   theme: 'auto', // Automatic theme detection
 
-  // App root data
+  view: {
+    stackPages: true, // For navigation between multi-level pages
+  },
   data: function () {
     return {
       // App root data
@@ -61,7 +63,7 @@ const setUpThreads = (data) => {
     const li = `
       <li class="swipeout">
         <div class="swipeout-content">
-          <a href="/thread/" class="item-link item-content">
+          <a href="/thread/${doc.id}/" data-thread-id="${doc.id}" class="item-link item-content thread-details">
             <div class="item-media"><img src="http://placehold.jp/40x40.png" width="44"/></div>
             <div class="item-inner">
               <div class="item-title-row">
@@ -81,7 +83,27 @@ const setUpThreads = (data) => {
   threadsList.innerHTML = html;
 }
 
-// Pop ups with swipe to close
+// Setting up the thread detail
+const setUpThreadDetails = (id) => {
+  db.collection('threads')
+    .where(firebase.firestore.FieldPath.documentId(), "==", id)
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const thread = doc.data();
+        document.getElementById("thread-title").innerText = thread.title;
+        document.getElementById("thread-description").innerText = thread.description;
+      });
+    });
+}
+
+// Event listeners
+$$(document).on('click', '.thread-details', function () {
+  const id = $$(this).data('thread-id');
+  setUpThreadDetails(id);
+})
+
+// Pop up with swipe to close
 const loginSwipeToClosePopup = app.popup.create({
   el: '.login-popup',
   swipeToClose: true,
@@ -89,7 +111,7 @@ const loginSwipeToClosePopup = app.popup.create({
 
 // Dialogs 
 // Open a dialog for adding a new thread
-$$('.new-thread-dialog').on("click", function () {
+$$('.new-thread-dialog').on('click', function () {
   app.dialog.create({
     content: ' <div class="page-content login-screen-content"> <div class="block-title">New Topic</div> <form class="list" id="create-thread-form"> <div class="list"> <ul> <li class="item-content item-input"> <div class="item-inner"> <div class="item-input-wrap"> <input type="text" id="title" name="title" placeholder="Thread Title" required validate/> </div> </div> </li> <li class="item-content item-input"> <div class="item-inner"> <div class="item-input-wrap"> <textarea id="description" name="description" placeholder="Description" required validate></textarea> </div> </div> </li> </ul> </div> <div class="row display-flex justify-content-center"> <a class="button" id="create-thread" href="#">Create Thread</a> <a class="button" id="cancel-thread" href="#">Cancel</a> </div> </form> </div>',
     cssClass: 'dialog'
@@ -99,12 +121,12 @@ $$('.new-thread-dialog').on("click", function () {
 });
 
 // Close the thread dialog
-$$(document).on("click", "#cancel-thread", function () {
+$$(document).on('click', '#cancel-thread', function () {
   app.dialog.close();
 });
 
 // Open a dialog for adding a new comment
-$$(document).on("click", ".new-comment-dialog", function () {
+$$(document).on('click', '.new-comment-dialog', function () {
   app.dialog.create({
     content: '<div class="page-content login-screen-content"> <form class="list"> <div class="list"> <ul> <li class="item-content item-input"> <div class="item-inner"> <div class="item-input-wrap"> <textarea name="description" placeholder="Write your comment here"></textarea> </div> </div> </li> </ul> </div> <div class="row display-flex justify-content-center"> <a class="button popup-close submit-comment-dialog" href="#">Submit</a>  <a class="button submit-comment-dialog" href="#">Cancel</a></div> </form> </div>',
     cssClass: 'dialog'
@@ -112,19 +134,19 @@ $$(document).on("click", ".new-comment-dialog", function () {
 });
 
 // After submission, close comment dialog
-$$(document).on("click", ".submit-comment-dialog", function () {
+$$(document).on('click', '.submit-comment-dialog', function () {
   app.dialog.close();
 });
 
 // Confirmation dialog for deleting a thread
-$$(document).on("click", ".delete-thread-dialog", function () {
+$$(document).on('click', '.delete-thread-dialog', function () {
   app.dialog.confirm(' Are you sure you want to delete the thread?', '', function () {
     app.dialog.alert('Thread Deleted', '');
   });
 });
 
 // Confirmation dialog for deleting a comment
-$$(document).on("click", ".delete-comment-dialog", function () {
+$$(document).on('click', '.delete-comment-dialog', function () {
   app.dialog.confirm(' Are you sure you want to delete the comment?', '', function () {
     app.dialog.alert('Comment Deleted', '');
   });
